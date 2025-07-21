ec73bd3c6f2925915cef5115c8cddf38a3485ea0
rtdevcraft <hiiamabat@gmail.com>
2025-07-21 13:13:27 -0600

    Edited next config for 'standalone'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // This option is crucial for Docker deployments.
+  // It creates a minimal server with only the necessary files to run the app,
+  // which is what our Dockerfile is designed to use.
+  output: 'standalone',
+}
 
-export default nextConfig;
+export default nextConfig
